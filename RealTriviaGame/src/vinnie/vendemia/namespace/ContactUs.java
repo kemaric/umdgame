67765200ef9ae3acb0607c4047dbf8d9eb89a8b8
ooludemi@terpmail.umd.edu <ooludemi@terpmail.umd.edu@f9678f0c-a260-6985-c2a9-1599a2bbd58b>
2012-06-13 05:35:44 +0000

    Hey guys I made some more changes they're listed below: 1) I decided we need a "contact us" activity which I have added, you can view it to see if it needs more work 2) I changed the layout of the home screen a little bit by adding another button that goes to the contact us activity 3) All these changes also made me alter the xml file; I'm not going into much details about what I changed. If you guys dont like the idea of a contact us activity feel free to take it off.  else if you think it needs more work let me know

---
+++ RealTriviaGame/src/vinnie/vendemia/namespace/ContactUs.java
@@ -0,0 +1,35 @@
+package vinnie.vendemia.namespace;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+/**
+ * This class defines the contact Us activity.  This will enable users of our app
+ * to email us bugs and it is also a way to get more people into the club because 
+ * our contact info is on this activity.  This activity contains a button that will
+ * navigate them back to the home screen and a text that tells them our email
+ * address
+ * @author bori
+ *
+ */
+public class ContactUs extends Activity {
+Button backToHome;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.contact);
+		backToHome = (Button) findViewById(R.id.HomeButton);
+		backToHome.setOnClickListener(new View.OnClickListener() {
+			/*this will enable the user to go to the home screen when they're
+			 * done viewing this activity*/
+			public void onClick(View goHomeView) {
+				// TODO Auto-generated method stub
+				Intent goBackHome = new Intent("vinnie.vendemia.namespace.HOMESCREEN");
+				startActivity(goBackHome);
+			}
+		});
+	}
+}
--- RealTriviaGame/src/vinnie/vendemia/namespace/HomeScreen.java
+++ RealTriviaGame/src/vinnie/vendemia/namespace/HomeScreen.java
@@ -7,11 +7,9 @@ import android.view.View;
 import android.widget.Button;
 
 
-
-
 public class HomeScreen extends Activity {
 
-	Button startQuiz;
+	Button startQuiz,contactUsActivity;
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,6 +20,17 @@ public class HomeScreen extends Activity {
 				Intent openTriviaGame = new 
 						Intent("vinnie.vendemia.namespace.REALTRIVIAGAMEACTIVITY");
 				startActivity(openTriviaGame);
+			}
+		});
+		/*I added the "contact us" button to the home screen that way they can navigate to 
+		 *the contact activity*/
+		contactUsActivity = (Button) findViewById(R.id.contactButton);
+		contactUsActivity.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View contactView) {
+				// TODO Auto-generated method stub
+			Intent goToContact = new Intent("vinnie.vendemia.namespace.CONTACTUS");
+			startActivity(goToContact);
 			}
 		});
 	}
