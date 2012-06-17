447bd10c195b07692edd95fdda93ad5906bb30b0
dylanzingler@gmail.com <dylanzingler@gmail.com@f9678f0c-a260-6985-c2a9-1599a2bbd58b>
2012-06-17 21:35:22 +0000

    Added a font to the assets folder and used it on the home screen page. also added a string value called Banner for what you want to be displayed on the home screen

--- trunk/RealTriviaGame/src/vinnie/vendemia/namespace/HomeScreen.java
+++ trunk/RealTriviaGame/src/vinnie/vendemia/namespace/HomeScreen.java
@@ -1,19 +1,35 @@
 package vinnie.vendemia.namespace;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class HomeScreen extends Activity {
 
 	Button startQuiz,contactUsActivity;
+	Typeface font;
+	Context context;
+	TextView banner;
+	
+	public void ContentManager(Context context) {
+		// Add all of the variables to manage the content such as images and
+		// fonts to this method
+		
+	}
 	
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+		font = Typeface.createFromAsset(getAssets(), "Dragon.ttf");
+		banner = (TextView) findViewById(R.id.tvBanner);
+		banner.setText(getString(R.string.Banner));
+		banner.setTypeface(font, Typeface.BOLD);
 		startQuiz = (Button) findViewById(R.id.startButton);
 		startQuiz.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
